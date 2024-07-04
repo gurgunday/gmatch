93ae15ca091c634ae8d1109cc89c5a5464913a96
Gürgün Dayıoğlu <hey@gurgun.day>
2024-07-04 10:10:54 +0300

    add desc

--- src/index.js
+++ src/index.js
@@ -86,8 +86,8 @@ const Match = class extends Writable {
     this.processedBytes += difference + 1;
     this.buffer =
       patternLength === 1
-        ? Buffer.alloc(0)
-        : buffer.subarray(-patternLastIndex);
+        ? Buffer.alloc(0) // No need to keep the buffer if the pattern is a single byte
+        : buffer.subarray(-patternLastIndex); // Keep the last patternLength - 1 bytes for potential match
   }
 };
 
