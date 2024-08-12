c4d9c3c4963f0329b09f03d8e232683202866e2a
Gürgün Dayıoğlu <hey@gurgun.day>
2024-08-12 09:01:56 +0300

    feat: browser compatibility (#6)
    * improve algorithm
    
    * simplify
    
    * use Math.min

--- bench/index.js
+++ bench/index.js
@@ -1,17 +1,16 @@
 /* eslint-disable unicorn/no-array-push-push */
 
-"use strict";
-
-const { Bench } = require("tinybench");
-const { Match } = require("../src/index.js");
-const StreamSearch = require("streamsearch");
+import { Match } from "../src/index.js";
+import StreamSearch from "streamsearch";
+import { Bench } from "tinybench";
+import { Buffer } from "node:buffer";
 
 const bench = new Bench({ time: 2500 });
-const pattern = "example";
+const pattern = "exampleexampleexampleexampleexampleexample";
 const longText =
   `This is a long text with multiple occurrences of the word example. ` +
-  `It repeats the word example several times to ensure we have enough ${"data for the benchmark. Here's another example.".repeat(
-    1000,
+  `It repeats the word example several times to ensure we have enough ${"data for the benchmark. Here's another exampleexampleexampleexampleexampleexample.".repeat(
+    10,
   )}`;
 
 let gmatchMatches;
@@ -20,13 +19,21 @@ let streamsearchMatches;
 bench
   .add("gmatch", () => {
     const matches = [];
-    const search = new Match(pattern, (index) => {
-      matches.push(index);
-    });
+    const search = new Match(
+      pattern,
+      (isMatch, data, start) => {
+        if (isMatch) {
+          matches.push(start);
+        }
+      },
+      Buffer.from,
+    );
 
+    search.write("exampleexampleexampleexampleexample");
+    search.write("exampleexampleexampgeexampleexampleexample");
+    search.write("exampleexampleexampgeexampleexampleexample");
     search.write(longText);
-    search.write(longText);
-    search.write(longText);
+    search.write(pattern);
 
     gmatchMatches = matches.length;
   })
@@ -38,9 +45,11 @@ bench
       }
     });
 
+    search.push("exampleexampleexampleexampleexample");
+    search.push("exampleexampleexampgeexampleexampleexample");
+    search.push("exampleexampleexampgeexampleexampleexample");
     search.push(longText);
-    search.push(longText);
-    search.push(longText);
+    search.push(pattern);
 
     streamsearchMatches = matches.length;
   });
